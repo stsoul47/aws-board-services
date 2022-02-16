b81168baf86c3b28ff43aeda3ba2e8f8752f40b7
stsoul47 <stsoul47@naver.com>
2022-02-16 21:09:06 +0900

    swagger INIT and swagger config settings

---
+++ aws-board-api-server/src/main/java/com/jslee/awsboardapiserver/project/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.jslee.awsboardapiserver.project.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/api/hello")
+public class HelloController {
+    @Operation(summary = "test Hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true,example = "Lee") @RequestParam String name) {
+        return ResponseEntity.ok("Hello " + name);
+    }
+}
