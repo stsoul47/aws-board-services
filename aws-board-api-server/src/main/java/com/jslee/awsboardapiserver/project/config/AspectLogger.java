6c6e05bd1129aeb4d20ede7f7cfc15366fc07b28
stsoul47 <stsoul47@naver.com>
2022-02-18 23:04:17 +0900

    AOP Logger Config 수정

--- aws-board-api-server/src/main/java/com/jslee/awsboardapiserver/project/config/AspectLogger.java
+++ aws-board-api-server/src/main/java/com/jslee/awsboardapiserver/project/config/AspectLogger.java
@@ -3,13 +3,14 @@ package com.jslee.awsboardapiserver.project.config;
 import com.jslee.awsboardapiserver.project.common.ResponseData;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -18,5 +19,40 @@ import java.util.stream.IntStream;
 @Component
 @Slf4j
 public class AspectLogger {
+    // com.aop.controller 이하 패키지의 모든 클래스 이하 모든 메서드에 적용
+    @Pointcut("execution(* com.jslee.awsboardapiserver.project.controller.*.*(..))")
+    private void cut(){}
 
+    // Pointcut에 의해 필터링된 경로로 들어오는 경우 메서드 호출 전에 적용
+    @Before("cut()")
+    public void beforeParameterLog(JoinPoint joinPoint) {
+        // 메서드 정보 받아오기
+        Method method = getMethod(joinPoint);
+        log.info("======= method name = {} =======", method.getName());
+
+        // 파라미터 받아오기
+        Object[] args = joinPoint.getArgs();
+        if (args.length <= 0) log.info("no parameter");
+        for (Object arg : args) {
+            log.info("parameter type = {}", arg.getClass().getSimpleName());
+            log.info("parameter value = {}", arg);
+        }
+    }
+
+    // Poincut에 의해 필터링된 경로로 들어오는 경우 메서드 리턴 후에 적용
+    @AfterReturning(value = "cut()", returning = "returnObj")
+    public void afterReturnLog(JoinPoint joinPoint, Object returnObj) {
+        // 메서드 정보 받아오기
+        Method method = getMethod(joinPoint);
+        log.info("======= method name = {} =======", method.getName());
+
+        log.info("return type = {}", returnObj.getClass().getSimpleName());
+        log.info("return value = {}", returnObj);
+    }
+
+    // JoinPoint로 메서드 정보 가져오기
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
 }
